ec5e29b53419c0248f465b4c1364db869d4d1cd9
Francisco Cicerchia <francicerchia@gmail.com>
2021-06-08 18:07:25 -0300

    Cambios Totales

--- src/Library.hs
+++ src/Library.hs
@@ -19,7 +19,7 @@ type Calorias = Number
 
 quemarCalorias :: Gimnasta -> Calorias -> Gimnasta
 quemarCalorias (Gimnasta nombre edad peso tonificacion) calorias
-                | peso > 100 = (Gimnasta nombre edad (peso - div calorias 150) tonificacion)
+                | peso > 100 = (Gimnasta nombre edad (peso - calorias/150) tonificacion)
                 | edad > 30 && calorias > 200 = (Gimnasta nombre edad (peso - 1) tonificacion)
                 | otherwise = (Gimnasta nombre edad (peso - calorias/(peso*edad)) tonificacion)
 
@@ -52,5 +52,63 @@ colina inclinacion minutos gimnasta = quemarCalorias gimnasta (2*minutos*inclina
 
 --d 
 
---montania :: Inclinacion -> Minutos -> Ejercicio
---montania inclinacion minutos gimnasia = 
+montania :: Inclinacion -> Minutos -> Ejercicio
+montania inclinacion minutos = incrementarTonificacion.(colina (inclinacion+3) (minutos/2)).(colina inclinacion (minutos/2))
+
+incrementarTonificacion :: Ejercicio
+incrementarTonificacion (Gimnasta nombre edad peso tonificacion) = (Gimnasta nombre edad peso (tonificacion + 1))
+
+--PUNTO 4
+
+--a
+
+data Rutina = UnaRutina{
+    nombre :: String,
+    duracion :: Number,
+    ejercicios :: [Ejercicio]
+}
+
+completo = UnaRutina{
+    nombre = "Completo",
+    duracion = 50,
+    ejercicios = [colina 5 40,pesas 50 15]
+}
+
+--i
+
+--ii
+
+realizarRutina :: Rutina -> Ejercicio
+realizarRutina rutina = realizarEjercicios (ejercicios rutina)
+
+realizarEjercicios :: [Ejercicio] -> Gimnasta -> Gimnasta
+realizarEjercicios [] gimnasta = gimnasta 
+realizarEjercicios (ejercicio : ejercicios) gimnasta = realizarEjercicios ejercicios (ejercicio gimnasta)
+
+
+--iii
+
+realizarRutina' :: Rutina -> Ejercicio
+realizarRutina' rutina gimnasta = foldl realizarEjercicio' gimnasta (ejercicios rutina)
+
+realizarEjercicio' :: Gimnasta -> Ejercicio -> Gimnasta
+realizarEjercicio' gimnasta ejercicio = ejercicio gimnasta
+
+--b
+
+resumenDeRutina :: Rutina -> Gimnasta -> (String,Number,Number)
+resumenDeRutina rutina gimnasta = (nombre rutina , obtenerKilosPerdidos (realizarRutina rutina gimnasta) gimnasta , obtenerTonificacionGanada (realizarRutina rutina gimnasta) gimnasta )
+
+obtenerKilosPerdidos :: Gimnasta -> Gimnasta -> Number
+obtenerKilosPerdidos (Gimnasta nombre edad peso tonificacion) (Gimnasta nombreOriginal edadOriginal pesoOriginal tonificacionOriginal) = pesoOriginal - peso
+
+obtenerTonificacionGanada :: Gimnasta -> Gimnasta -> Number
+obtenerTonificacionGanada (Gimnasta nombre edad peso tonificacion) (Gimnasta nombreOriginal edadOriginal pesoOriginal tonificacionOriginal) = tonificacion - tonificacionOriginal
+
+--PUNTO 5
+
+rutinasSaludable :: [Rutina] -> Gimnasta -> [Rutina]
+rutinasSaludable rutinas gimnasta = filter (esRutinaSaludable gimnasta) rutinas
+
+esRutinaSaludable :: Gimnasta -> Rutina -> Bool
+esRutinaSaludable gimnasta rutina = saludable(realizarRutina rutina gimnasta)
